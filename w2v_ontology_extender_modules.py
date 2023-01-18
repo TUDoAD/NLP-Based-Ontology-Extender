ec57f26d0e5052636071cbebe4972b956bd38ea5
AlexB <55703624+AleSteB@users.noreply.github.com>
2023-01-18 17:54:26 +0100

    Update w2v_ontology_extender_modules.py

--- w2v_ontology_extender_modules.py
+++ w2v_ontology_extender_modules.py
@@ -994,53 +994,68 @@ def ontology_class_extender(ontology_filenames = ["SBO"],
 
 ##
 # Annotates classes in extended ontology with definitions
+# 
 # AB HIER WEITER
 ##
     
-def ontology_class_annotator(files_to_extend = ["./"]):
-    ontology_filename_list = ["./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc1_0.999","./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc2_0.999","./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc5_0.999","./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc10_0.999","./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc25_0.999",
-                              "./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc10_0.99","./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc10_0.995"]
+def ontology_class_annotator(ontology_files_to_extend = ["./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc1_0.999"],
+                            ontology_filenames = ["Allotrope_OWL"],
+                            use_IUPAC_goldbook = True,
+                            provenance_string = "AB",
+                            mute_prints = True):
+    """
     
-    for ontology_filename in ontology_filename_list:
+
+    Parameters
+    ----------
+    ontology_files_to_extend : TYPE, optional
+        DESCRIPTION. The default is ["./ontologies_output/Allotrope_OWL_ext_methanation_only_text_mc1_0.999"].
+    ontology_filenames : TYPE, optional
+        DESCRIPTION. The default is ["Allotrope_OWL"].
+    use_IUPAC_goldbook : TYPE, optional
+        DESCRIPTION. The default is True.
+    provenance_string : TYPE, optional
+        DESCRIPTION. The default is "AB".
+    mute_prints : TYPE, optional
+        DESCRIPTION. The default is True.
+
+    Returns
+    -------
+    metrics_dict : TYPE
+        DESCRIPTION.
+
+    """
+    json_filenames = []
+    
+    for ontology_filename in ontology_files_to_extend:
         
         local_world = owlready2.World()
         onto_local = local_world.get_ontology(ontology_filename + ".owl").load()
+        
+        # list_classes will contain all the subclasses of w2vConcept: these are 
+        # the classes automatically introduced by Word2Vec, that were not already 
+        # contained in the extended ontology
         list_classes = list(onto_local.get_children_of(onto_local.w2vConcept))
         
-        # Labels einfügen
+        # instert labels
         for i_1, i_2 in enumerate(list_classes):
             if i_2.label:
-                print("Label zu '{}' bereits definiert".format(i_2.label[0]))
+                print("Something went wrong? Label for '{}' already defined".format(i_2.label[0]))
             else:
                 list_classes[i_1].label = i_2.name.replace("_"," ").lower()
         
         onto_local.save(file = ontology_filename + "_output.owl")
         
-        
-        
-        ######
-        
         label_list = [i.label[0] for i in list_classes]
-        
         labels_to_classes_dict = {i.label[0] : i for i in list_classes}
+
+        [class_dict, desc_dict] = onto_loader(ontology_filenames)
         
-        [class_dict, desc_dict] = onto_loader(["chmo","Allotrope_OWL", "chebi", "NCIT", "bao_complete_merged", "SBO"])
         
-        
-        ## LOADING IUPAC GOLDBOOK 
-        temp_dict = {}
-        with open('./ontologies/goldbook_vocab.json', encoding = "utf8") as json_file:
-            dict_data = json.load(json_file)
-            for entry in dict_data["entries"].keys():
-                if dict_data["entries"][entry]["term"] != None:
-                    if dict_data["entries"][entry]["definition"] != None:
-                        temp_dict[dict_data["entries"][entry]["term"].lower()] = dict_data["entries"][entry]["definition"]
-                    else:
-                        print("empty definition for term: {}".format(dict_data["entries"][entry]["term"]))
-                        temp_dict[dict_data["entries"][entry]["term"].lower()] = "[AB] Class with same label also contained in [IUPAC-Goldbook]"
-                else:
-                    print("empty entry: {}".format(dict_data["entries"][entry]))
-        desc_dict["IUPAC-Goldbook"] = temp_dict
+        # LOADING IUPAC GOLDBOOK 
+        if use_IUPAC_goldbook:
+           desc_dict["IUPAC-Goldbook"] = IUPAC_goldbook_loader(mute_prints)
+        #
         
         resDict = {}
         for loaded_onto in desc_dict:
@@ -1053,81 +1068,49 @@ def ontology_class_annotator(files_to_extend = ["./"]):
                     if newlist: # entry found
                         summary.append(newlist)
                 except:
-                    print("Passed '{}', Ontology: {}".format(i,loaded_onto))
+                    if not mute_prints:
+                        print("Passed '{}', Ontology: {}".format(i,loaded_onto))
             resDict[loaded_onto] = summary
-        ## in rdfs:seeAlso oder so schreiben, dass mapping zu Ontologie XX und Klasse YY möglich ist, tagging mit [AB] oder so?
-        with open('FoundClasses.json', 'w') as jsonfile:
-            json.dump(resDict, jsonfile)
         
         onto_local = local_world.get_ontology(ontology_filename + "_output.owl").load()
-        #class_candidates = list(onto_local.get_children_of(onto_local.w2vConcept))
-        #class_dict_locOntos = {i.name : i for i in class_candidates}
+        
+        # add annotation string(s) as rdfs:comment
         with onto_local:
             for loaded_onto in resDict:
                 for classlabel in resDict[loaded_onto]: # list of classes        
                     defstring = ''.join(desc_dict[loaded_onto][classlabel[0]]) if desc_dict[loaded_onto][classlabel[0]] != classlabel[0] else "" 
                     if defstring:
-                        comment_string = defstring + "\nFound by [AB] in [" + loaded_onto + "]"
-                        print("def of {} found in ontology {}".format(classlabel[0], loaded_onto))
+                        comment_string = defstring + "\nFound by ["+ provenance_string +"] in [" + loaded_onto + "]"
+                        if not mute_prints:
+                            print("def of {} found in ontology {}".format(classlabel[0], loaded_onto))
                     else:
-                        comment_string = "[AB] Class with same label also contained in [{}] unable to obtain definition".format(loaded_onto)
-                    
+                        comment_string = "["+provenance_string+"] Class with same label also contained in [{}] unable to obtain definition string".format(loaded_onto)
+                   
+                    # search the class with label according to classlabel and insert comment string as rdfs:comment
                     onto_local.search_one(label = classlabel[0]).comment.append(comment_string)
-                    #class_dict_locOntos[classlabel[0]].comment.append(comment_string)    
-                    #w2v_class.comment.append(comment_string)
             
+            # list number of unique classes added via word2vec as subclass of w2vConcept
             w2vSubclasses = len(set(list(onto_local.w2vConcept.subclasses())))
+            # store modified ontology in ./ontologies_output/ 
             onto_local.save(file = ontology_filename + "_output.owl")
-            
-        '''
-        ## storing definitions in ontology
-        for loaded_onto in resDict:
-            for concept in resDict[loaded_onto]:
-                temp_class = labels_to_classes_dict[concept[0]]
-                # defstring should contain definition contained in desc_dict but only 
-                # IF the definition is not equal to the label
-                # if the definition is equal to the label -> no definition was contained in ontology
-                if loaded_onto == "IUPAC-Goldbook":
-                    defstring = ''.join(desc_dict[loaded_onto][concept[0]]) if desc_dict[loaded_onto][concept[0]] != concept[0] else "" 
-                else:            
-                    defstring = ''.join(desc_dict[loaded_onto][concept[0]]) if desc_dict[loaded_onto][concept[0]] != concept[0] else "" 
-                
-                if defstring:
-                    comment_string =  defstring + "\nFound by [AB] in [" + loaded_onto + "]"
-                else:
-                    comment_string = "[AB] Class with same label also contained in [{}]".format(loaded_onto)
-               #print("{}: {}\n".format(concept[0],comment_string))
-                temp_class.comment.append(comment_string)
-        '''
         
-        # Found concepts (unique)
+        # List found concepts and make set of unique ones
         tempVar = []
         for key in resDict:
             for i in resDict[key]: tempVar.append(i[0])    
         found_concepts = set(tempVar)
         resDict["unique_keys"] = len(found_concepts)
         resDict["new_classes"] = w2vSubclasses
-        with open(ontology_filename + "_new_classes.json", 'w') as f:
+        with open("./json-files/"+ ontology_filename + "_new_classes.json", 'w') as f:
                 json.dump(resDict, f)
-        #andereVariableTest = list_classes[5]
-        # andereVariableTest.name
-        # .title()
         
-    import json
-    import pandas as pd
+        json_filenames.append("./json-files/"+ ontology_filename + "_new_classes.json")
     
-    json_filenames = ["mc1_0.999_new_classes.json",
-    "mc10_0.995_new_classes.json",
-    "mc10_0.99_new_classes.json",
-    "mc25_0.999_new_classes.json",
-    "mc10_0.999_new_classes.json",
-    "mc5_0.999_new_classes.json",
-    "mc2_0.999_new_classes.json"]
-    
-    df_dict = {}
+    ## Export metrics of extended ontologies
+    metrics_dict = {}
     
     for filename in json_filenames:
-        with open('./ontologies_output/Allotrope_OWL_ext_methanation_only_text_'+filename, 'r') as f:
+        with open(filename, 'r') as f:
             data = json.load(f)
         tempdict = {}
         for key in data:
@@ -1136,7 +1119,13 @@ def ontology_class_annotator(files_to_extend = ["./"]):
             else:
                 tempdict[key] = len(data[key])
         
-        df_dict[filename] = tempdict
+        metrics_dict[filename] = tempdict
         
-    df = pd.DataFrame(df_dict)
-    df.to_excel('Auswertung_Onto_extension_defCounts.xlsx')
+    df = pd.DataFrame(metrics_dict)
+    print("=============================================")
+    
+    df.to_excel("./xlsx-files/Metrics_{}_annotation_unique_keys.xlsx".format(provenance_string))
+    print("Stored metrics for all min_count paramters in ./xlsx-files/Metrics_{}_annotation_unique_keys.xlsx".format(provenance_string))
+    print("=============================================")
+    
+    return metrics_dict
